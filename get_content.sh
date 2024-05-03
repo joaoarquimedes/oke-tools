93ad95b2b0cd68441229575731ba46d0122e3ecf
joao.costa <joao.costa@vanelos.com.br>
2024-05-03 17:16:41 -0300

    Update tools

--- get_content.sh
+++ get_content.sh
@@ -6,17 +6,15 @@ NAMESPACE="<__NAMESPACE_NAME__>"
 # List all types of resources in the specified namespace
 for resource in $(kubectl api-resources --namespaced=true --verbs=list --output=name)
 do
-  echo "=== $resource ==="
   if ! kubectl get --namespace $NAMESPACE $resource 2>&1 | grep "No resources found" > /dev/null
   then
+    echo "=== $resource ==="
+
     # Create folder resource name
     [[ ! -d $resource ]] && mkdir $resource
 
     # Get all resource content
-    function get_r_name(){
-      kubectl get --namespace $NAMESPACE $resource -o custom-columns=NAME:.metadata.name
-    }
-    for i in $(get_r_name | grep -v "NAME")
+    for i in $(kubectl get --namespace $NAMESPACE $resource -o custom-columns=NAME:.metadata.name | grep -v "NAME")
     do
      echo "--> Save ${resource}/${i}.yml"
      kubectl get --namespace $NAMESPACE $resource ${i} -o yaml > ${resource}/${i}.yml
